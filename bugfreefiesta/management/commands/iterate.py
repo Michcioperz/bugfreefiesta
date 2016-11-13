0cb7e1eabec621b3e46dc819319fb3622cc2900f
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-11-13 10:21:59 +0100

    at least it compiles now

--- bugfreefiesta/management/commands/iterate.py
+++ bugfreefiesta/management/commands/iterate.py
@@ -1,6 +1,12 @@
 from django.core.management.base import BaseCommand
 from bugfreefiesta.models import Task, Test, PinTest, Submission, Result, PinResult
+import subprocess, os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        for to_compile in Submission.objects.filter(compiled=False):
+            compiler = subprocess.run(['g++', '-x', 'c++', '-std=c++11', '-O2', '-o', os.path.join(os.getenv("VIRTUALENV", "/home/michcioperz"), "compiled", "{}.o".format(to_compile.pk)), '-'], universal_newlines=True, input=to_compile.code, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            to_compile.code = ""
+            to_compile.compiled = True
+            to_compile.compilator_output = "Return code {}\n".format(compiler.returncode) + compiler.stdout.read()
+            to_compile.save()
