2ccb8110a7cc38917bff2cba90d2bc9982105fe9
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-11-13 20:33:21 +0100

    this is gonna kill me sometime soon

--- bugfreefiesta/management/commands/iterate.py
+++ bugfreefiesta/management/commands/iterate.py
@@ -5,7 +5,7 @@ import subprocess, os
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for to_compile in Submission.objects.filter(compiled=False):
-            compiler = subprocess.run(['g++', '-x', 'c++', '-std=c++11', '-O2', '-o', os.path.join(os.getenv("VIRTUALENV", "/home/michcioperz"), "compiled", "{}.o".format(to_compile.pk)), '-'], universal_newlines=True, input=to_compile.code, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            compiler = subprocess.run(['g++', '-x', 'c++', '-std=c++11', '-O2', '-o', os.path.join(os.getenv("VIRTUAL_ENV", "/home/michcioperz"), "compiled", "{}.o".format(to_compile.pk)), '-'], universal_newlines=True, input=to_compile.code, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             to_compile.code = ""
             to_compile.compiled = True
             to_compile.compilator_output = "Return code {}\n".format(compiler.returncode) + compiler.stdout
