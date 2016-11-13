70dc3b447e8a9334d1014234fb5734806d4073cb
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-11-13 21:56:34 +0100

    out the door

--- bugfreefiesta/urls.py
+++ bugfreefiesta/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from .views import task_submit, task_detail, test_detail, pin_test_detail, submission_detail, result_detail, pin_result_detail, task_list
 
