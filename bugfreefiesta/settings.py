6eaa6e9b03a6ff6ed5b140e29f45673ad397a4d3
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-11-13 09:27:53 +0100

    my mistake

--- bugfreefiesta/settings.py
+++ bugfreefiesta/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.getenv("VIRTUALENV", BASE_DIR), "static_dump")
+STATIC_ROOT = os.path.join(os.getenv("VIRTUAL_ENV", BASE_DIR), "static_dump")
